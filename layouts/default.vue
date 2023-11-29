7f7db9e36dbe0b7b583389e47e732f7e441b2225
RyanP0612 <128430984+RyanP0612@users.noreply.github.com>
2023-11-29 19:56:00 -0300

    Add files via upload

---
+++ layouts/default.vue
@@ -0,0 +1,8 @@
+<template>
+    <div>
+        <img src="http://127.0.0.1:8000/media/simbolo.png" alt="">
+       
+        <Menu></Menu>
+        <slot></slot>
+    </div>
+</template>
---
+++ pages/ambientes/index.vue
@@ -0,0 +1,12 @@
+<script>
+
+</script>
+
+
+<template>
+    <div>
+        <h1>
+            bem vindo aos ambientes
+        </h1>
+    </div>
+</template>
