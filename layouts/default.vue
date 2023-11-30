4543f291d017ebff1c46a1749d69f0e999204d6e
RyanP0612 <ryansalva0612@gmail.com>
2023-11-29 22:18:40 -0300

    ajuste

--- layouts/default.vue
+++
@@ -1,8 +0,0 @@
-<template>
-    <div>
-        <img src="http://127.0.0.1:8000/media/simbolo.png" alt="">
-       
-        <Menu></Menu>
-        <slot></slot>
-    </div>
-</template>
--- layouts/loginlay.vue
+++
@@ -1,9 +0,0 @@
-
-<template>
-    <div>
-        <img src="https://avatars.githubusercontent.com/u/89030432?v=4" alt="">
-        <br>
-        <hr>
-        <slot></slot>
-    </div>
-</template>
